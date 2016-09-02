77d22bbc973ca3a00e8ee7daeedaffbc8ed5dd8a
abcdefz <ztz_sjtu@163.com>
2016-09-02 14:18:17 +0800

    add a new page called cubewithpng

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,6 +6,10 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('learnthree.html')
+	
+@app.route('/cubewithpng', methods=['GET', 'POST'])
+def cubeWithPng():
+    return render_template('cubewithpng.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
